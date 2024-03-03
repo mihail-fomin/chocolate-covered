20abb09b131303f036879565469fa9d6a174ddeb
misha133kv <mihail_fomin_95@mail.ru>
2024-03-03 20:33:54 +0500

    Added fetch products

---
+++ app/api/categories/[category]/route.ts
@@ -0,0 +1,7 @@
+// import prisma from "@/app/utils/connect";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   const products = await prisma.product.findMany({})
+//   return NextResponse.json(products)
+// }
--- app/categories/[category]/page.tsx
+++ app/categories/[category]/page.tsx
@@ -1,9 +1,14 @@
+import prisma from '@/app/utils/connect'
 import React from 'react'
+interface Props {
+  params: { category: string }
+}
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const page = async ({ params }: Props) => {
+  const category = params.category
+  const products = await prisma.product.findMany({ where: { category } })
+
+  return <ul>{products.map((product) => product.title)}</ul>
 }
 
 export default page
