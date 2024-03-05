96fa5c1f348254220abdeb40b611949b79e5457a
misha133kv <mihail_fomin_95@mail.ru>
2024-03-05 21:45:39 +0500

    Added ScrollArea

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,24 +4,29 @@ import React from 'react'
 import categories from '../public/category.json'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Button } from '@radix-ui/themes'
+import { Box, Button, ScrollArea } from '@radix-ui/themes'
 import Container from './components/Container'
 
 const NavBar = () => {
   const currentPath = usePathname()
 
   return (
-    <ul className="grid grid-rows-3 grid-flow-col gap-2 mt-3 text-center md:grid-rows-2">
-      {categories.map((category) => (
-        <li key={category.href}>
-          <Button
-            variant={category.href === currentPath ? 'solid' : 'outline'}
-          >
-            <Link href={category.href}>{category.rus}</Link>
-          </Button>
-        </li>
-      ))}
-    </ul>
+    <ScrollArea type='always' scrollbars='horizontal' style={{height: '4rem'}}>
+      <Box p='2'>
+        <ul className="flex gap-3">
+          {categories.map((category) => (
+            <li key={category.href} className=''>
+              <Button
+                variant={category.href === currentPath ? 'solid' : 'outline'}
+                >
+                <Link href={category.href}>{category.rus}</Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </Box>
+
+    </ScrollArea>
   )
 }
 
