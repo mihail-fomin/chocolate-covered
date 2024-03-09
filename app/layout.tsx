3740b2ad22745f9ee8fbd69866f0b68de9024468
misha133kv <mihail_fomin_95@mail.ru>
2024-03-09 13:43:48 +0500

    Fix incorrect import

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,7 @@ import Header from './Header'
 import './globals.css'
 import '@radix-ui/themes/styles.css'
 import Container from './components/Container'
-import { Provider } from 'react-redux'
-import { store } from './StoreProvider'
+import StoreProvider from './StoreProvider'
 
 export const metadata: Metadata = {
   title: 'Все в шоколаде',
@@ -22,13 +21,13 @@ export default function RootLayout({
     <html lang="ru">
       <body>
         <Theme accentColor="ruby">
-          <Provider store={store}>
+          <StoreProvider>
             <Header />
             <Container>
               {children}
               {/* <ThemePanel /> */}
             </Container>
-          </Provider>
+          </StoreProvider>
         </Theme>
       </body>
     </html>
