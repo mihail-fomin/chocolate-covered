b70a3cb812d19456c009e5ad6797cbf25a1deceb
laptop <107790477+mihail-fomin@users.noreply.github.com>
2024-03-14 20:36:32 +0500

    Added Cart Modal

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,55 +3,23 @@
 import React from 'react'
 import Container from './components/Container'
 import Link from 'next/link'
-import { Button, Flex } from '@radix-ui/themes'
+import { Flex } from '@radix-ui/themes'
 import Image from 'next/image'
-// import { usePathname } from 'next/navigation'
-// import { useAppSelector } from './lib/hooks'
-// import { getDefaultValue } from './utils'
+import Cart from './components/Cart'
 
 const Header: React.FC = () => {
-  // const currentPath = usePathname()
-
-  // const activeCategory = useAppSelector(
-  //   (state) => state.category.activeCategory,
-  // )
-
-  // const links = [
-  //   { label: 'Главная', href: '/', name: 'home' },
-  //   {
-  //     label: 'Товары',
-  //     href: `/categories/${activeCategory}`,
-  //     name: 'categories',
-  //   },
-  // ]
 
   return (
     <header className="bg-red-300 py-2">
       <Container>
         <nav>
           <Flex justify="between" align="center">
-            {/* <Tabs.Root defaultValue={getDefaultValue(currentPath)}>
-              <Tabs.List>
-                {links.map((link) => (
-                  <Tabs.Trigger value={link.name} key={link.name}>
-                    <Link href={link.href}>{link.label}</Link>
-                  </Tabs.Trigger>
-                ))}
-              </Tabs.List>
-            </Tabs.Root> */}
             <Link href="/">
               <Image className="h-12 p-2" width={100} height={100} src="/images/logo.svg" alt="Логотип" />
             </Link>
-            <Link href="/cart">
-              <Button>Корзина</Button>
-            </Link>
+            <Cart />
           </Flex>
         </nav>
-        {/* <img
-          className="max-w-96 mx-auto mt-3"
-          src="/images/logo.svg"
-          alt="Логотип"
-        /> */}
       </Container>
     </header>
   )
---
+++ app/components/Cart.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React from 'react'
+import { Avatar, Button, Card, Dialog, Flex, Text } from '@radix-ui/themes'
+import { useAppSelector } from '../lib/hooks'
+import { getTotalPrice } from '../utils'
+
+const Cart = () => {
+  const products = useAppSelector((state) => state.cart.items)
+
+  return (
+    <Dialog.Root>
+    <Dialog.Trigger>
+      <Button>Корзина</Button>
+    </Dialog.Trigger>
+
+    <Dialog.Content>
+      <Dialog.Title>Корзина</Dialog.Title>
+
+      <Flex direction="column" gap="3">
+      <ul  className='flex flex-col gap-3'>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Flex direction='column' gap='3'>
+              
+            <Card>
+              <Flex gap="3" align="center">
+                <Avatar
+                  size="3"
+                  src={product.imageUrl}
+                  radius="full"
+                  fallback="T"
+                  />
+                <Flex className='w-full' justify='between' >
+                  <Text as="div" size="2" weight="bold">
+                    {product.title}
+                  </Text>
+                  <Text as="div" size="2" color="gray">
+                    {product.quantity} шт
+                  </Text>
+                </Flex>
+              </Flex>
+          </Card>
+            </Flex>
+          </li>
+        ))}
+        <Text>
+          Итого: <span>{getTotalPrice(products)}</span> ₽
+        </Text>
+      </ul>
+      </Flex>
+
+      <Flex gap="3" mt="4" justify="end">
+        <Dialog.Close>
+          <Button variant="soft" color="gray">
+            Закрыть
+          </Button>
+        </Dialog.Close>
+        <Dialog.Close>
+          <Button>Заказать</Button>
+        </Dialog.Close>
+      </Flex>
+    </Dialog.Content>
+  </Dialog.Root>
+  )
+}
+
+export default Cart
