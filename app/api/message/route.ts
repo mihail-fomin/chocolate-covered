854ed940100f2fdbc06727c3c0f1a6063d559c69
Mihail Fomin <m.fomin@quickresto.ru>
2024-03-21 10:49:54 +0500

    Extracted sendMessage method

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -1,28 +1,12 @@
-import TelegramBot from 'node-telegram-bot-api'
-import { CartItem } from '@/app/lib/feature/cart/cartSlice'
-import { IFormValues } from '@/app/components/Order/InputFields'
-
 import { NextRequest, NextResponse } from 'next/server'
+import { sendOrder } from '@/app/utils/telegram'
 
-const telegramToken = process.env.TG_TOKEN
-
-let bot: TelegramBot
-
-if (telegramToken) {
-  bot = new TelegramBot(telegramToken, { polling: true })
-}
-// Функция для отправки сообщения в Telegram
-export function sendTelegramMessage(message: string) {
-  const chatId = '-1001759583869'
-
-  bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
-}
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
     const { productArray, orderData } = body
-    const response = await sendOrder(productArray, orderData)
+    await sendOrder(productArray, orderData)
     return NextResponse.json({ success: 'Message sent' })
   } catch (error) {
     console.error(error)
@@ -30,53 +14,3 @@ export async function POST(req: NextRequest) {
   }
 }
 
-// Функция для обработки массива объектов и отправки сообщений
-export const sendOrder = async (
-  productArray: CartItem[],
-  orderData: IFormValues,
-) => {
-  try {
-    const {
-      name,
-      phone,
-      address,
-      format,
-      entrance,
-      floor,
-      intercom,
-      comments,
-    } = orderData
-    let totalSum = 0
-    let totalCount = 0
-    let message = `
-    <strong>Заявка с сайта</strong>
-    <b>Отправитель:</b> <i>${name}</i>
-    <b>Телефон:</b> <i>${phone}</i>
-    <b>Способ передачи:</b> ${format === 'delivery' ? `<i>Доставка</i> ` : `<i>Самовывоз</i>`}
-    ${
-      format === 'delivery'
-        ? `<b>Адрес:</b> <i>${address}</i>
-        <b>Подъезд:</b> <i>${entrance}</i>
-        <b>Этаж:</b> <i>${floor}</i>
-        <b>Домофон:</b> <i>${intercom}</i>`
-        : ``
-    }
-    ${comments.length > 0 ? `<b> Комментарии к заказу:</b> <i>${comments}</i>` : ``}
-    <b> Товары: </>
-    `
-
-    for (const productItem of productArray) {
-      const productAmount = productItem.quantity
-      message += `<b>${productItem.title}</> в количестве <b>${productAmount}</>шт., 
-      `
-      totalSum += Number(productItem.price) * Number(productAmount)
-      totalCount += Number(productAmount)
-    }
-
-    message += `<b> Итого: </> ${totalCount} товаров на сумму <b>${totalSum}</>руб.`
-
-    sendTelegramMessage(message)
-  } catch (error) {
-    console.error(error)
-  }
-}
--- app/utils/telegram.ts
+++ app/utils/telegram.ts
@@ -1,5 +1,4 @@
 import TelegramBot from 'node-telegram-bot-api'
-import { NextResponse } from 'next/server'
 import { CartItem } from '../lib/feature/cart/cartSlice'
 import { IFormValues } from '../components/Order/InputFields'
 
@@ -13,58 +12,58 @@ if (telegramToken) {
 
 // Функция для отправки сообщения в Telegram
 export function sendTelegramMessage(message: string) {
-  const chatId = '719127303'
-
-  bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+    const chatId = '-1001759583869'
+  
+    bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
 }
 
 // Функция для обработки массива объектов и отправки сообщений
 export const sendOrder = async (
-  productArray: CartItem[],
-  orderData: IFormValues,
-) => {
-  try {
-    const {
-      name,
-      phone,
-      address,
-      format,
-      entrance,
-      floor,
-      intercom,
-      comments,
-    } = orderData
-    let totalSum = 0
-    let totalCount = 0
-    let message = `
-    <strong>Заявка с сайта</strong>
-    <b>Отправитель:</b> <i>${name}</i>
-    <b>Телефон:</b> <i>${phone}</i>
-    <b>Способ передачи:</b> ${format === 'delivery' ? `<i>Доставка</i> ` : `<i>Самовывоз</i>`}
-    ${
-      format === 'delivery'
-        ? `<b>Адрес:</b> <i>${address}</i>
-        <b>Подъезд:</b> <i>${entrance}</i>
-        <b>Этаж:</b> <i>${floor}</i>
-        <b>Домофон:</b> <i>${intercom}</i>`
-        : ``
-    }
-    ${comments.length > 0 ? `<b> Комментарии к заказу:</b> <i>${comments}</i>` : ``}
-    <b> Товары: </>
-    `
-
-    for (const productItem of productArray) {
-      const productAmount = productItem.quantity
-      message += `<b>${productItem.title}</> в количестве <b>${productAmount}</>шт., 
+    productArray: CartItem[],
+    orderData: IFormValues,
+  ) => {
+    try {
+      const {
+        name,
+        phone,
+        address,
+        format,
+        entrance,
+        floor,
+        intercom,
+        comments,
+      } = orderData
+      let totalSum = 0
+      let totalCount = 0
+      let message = `
+      <strong>Заявка с сайта</strong>
+      <b>Отправитель:</b> <i>${name}</i>
+      <b>Телефон:</b> <i>${phone}</i>
+      <b>Способ передачи:</b> ${format === 'delivery' ? `<i>Доставка</i> ` : `<i>Самовывоз</i>`}
+      ${
+        format === 'delivery'
+          ? `<b>Адрес:</b> <i>${address}</i>
+          <b>Подъезд:</b> <i>${entrance}</i>
+          <b>Этаж:</b> <i>${floor}</i>
+          <b>Домофон:</b> <i>${intercom}</i>`
+          : ``
+      }
+      ${comments.length > 0 ? `<b> Комментарии к заказу:</b> <i>${comments}</i>` : ``}
+      <b> Товары: </>
       `
-      totalSum += Number(productItem.price) * Number(productAmount)
-      totalCount += Number(productAmount)
+  
+      for (const productItem of productArray) {
+        const productAmount = productItem.quantity
+        message += `<b>${productItem.title}</> в количестве <b>${productAmount}</>шт., 
+        `
+        totalSum += Number(productItem.price) * Number(productAmount)
+        totalCount += Number(productAmount)
+      }
+  
+      message += `<b> Итого: </> ${totalCount} товаров на сумму <b>${totalSum}</>руб.`
+  
+      sendTelegramMessage(message)
+    } catch (error) {
+      console.error(error)
     }
-
-    message += `<b> Итого: </> ${totalCount} товаров на сумму <b>${totalSum}</>руб.`
-
-    sendTelegramMessage(message)
-  } catch (error) {
-    console.error(error)
   }
-}
